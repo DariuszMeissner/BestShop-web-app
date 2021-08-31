7856dfc9ce5adefa78228205e27eb8bb54f0a3b0
DariuszMeissner <dariuszmeissner1@gmail.com>
2021-08-31 18:04:33 +0200

    setting up of webpack, updating of fonts and path of fonts

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+
+const path = require("path");
+const entryPath = ".";
+
+module.exports = {
+  entry: `./${entryPath}/js/app.js`,
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, `${entryPath}/build`),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, `${entryPath}`),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
